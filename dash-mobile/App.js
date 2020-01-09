147d7d3da98dde592fb1dc3502eceefd03888892
Henry Feng <henryfeng12345@gmail.com>
2020-01-09 11:51:31 -0600

    add basic components, styling, and layout for join page

--- dash-mobile/App.js
+++ dash-mobile/App.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.outerContainer}>
+      <View style={styles.topContainer}>
+        <Text style={styles.titleText}>Dash</Text>
+      </View>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.inputArea} placeholder="Name"/>
+        <TextInput style={styles.inputArea} placeholder="Chat Room"/>
+        <Button title="Go"/>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  outerContainer: {
+    backgroundColor: '#ffb65c',
+    flex:1
+  },
+  topContainer: {
+   padding: '35%',
+  },
+  titleText: {
+    textAlign: 'center'
+  },
+  inputContainer: {
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ffb65c'
+  },
+  inputArea: {
+    paddingBottom: 10
   },
 });
