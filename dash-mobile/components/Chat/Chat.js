25c3aa7c06ff19a1a11d5cd00da34c2649db9c1c
Henry Feng <henryfeng12345@gmail.com>
2020-01-12 14:46:34 -0600

    add state hooks and constants

--- dash-mobile/components/Chat/Chat.js
+++ dash-mobile/components/Chat/Chat.js
@@ -3,7 +3,56 @@ import { StyleSheet, Text, View, TextInput, Button, Modal } from 'react-native';
 import io from "socket.io-client";
 
 const Chat = ({navigation}) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [users, setUsers] = useState('');
+    //String og current message
+    const [message, setMessage] = useState('');
+    //Array of all messages of a chat instance
+    const [messages, setMessages] = useState([]);
+    //Socket Server connection
+    const ENDPOINT = 'https://dash-react-chat-app.herokuapp.com/'; 
 
+    // Socket connection
+    useEffect(() => {
+        
+    });
+    
+
+    // useEffect(() => {
+    //     const { name, room } = queryString.parse(location.search);
+    //     socket = io(ENDPOINT);
+    //     setRoom(room);
+    //     setName(name)
+    
+    //     socket.emit('join', { name, room }, (error) => {
+    //       if(error) {
+    //         alert(error);
+    //       }
+    //     });
+    //   }, [ENDPOINT, location.search]);
+    
+    //   //to handle messages.
+    //   useEffect(() => {
+    //         socket.on('message', (message) => {
+    //         //adding message to all messages
+    //         setMessages([...messages, message ]);
+    //         })
+    
+    //         socket.on('roomData', ({ users }) => {
+    //         setUsers(users);
+    //         })
+    
+    //         //disconnect event. Server will be listening for disconnect event
+    //         return () => {
+    //         socket.emit('disconnect');
+    //         socket.off();
+    //         }
+    //     // only run this useEffect when the messages array is changed
+    //   }, [messages])
+    
+
+    // handlers
     backToJoinHandler = () => {
         navigation.navigate('Join', {joinVisible: true});
     }
