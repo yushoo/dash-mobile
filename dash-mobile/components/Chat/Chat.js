ae94dc15a04caf6a1ea51f1c305b40fac418fc8d
Henry Feng <henryfeng12345@gmail.com>
2020-01-16 19:03:55 -0600

    find the issue for socket crash

--- dash-mobile/components/Chat/Chat.js
+++ dash-mobile/components/Chat/Chat.js
@@ -5,8 +5,8 @@ import io from "socket.io-client";
 let socket;
 
 const Chat = ({navigation}) => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
+    // const [name, setName] = useState('');
+    // const [room, setRoom] = useState('');
     const [users, setUsers] = useState('');
     //String og current message
     const [message, setMessage] = useState('');
@@ -17,33 +17,45 @@ const Chat = ({navigation}) => {
 
     // Socket 
     // Connection
+
+    // useEffect(() => {
+    //     socket = io(ENDPOINT);
+    // });
+
     useEffect(() => {
+        let tempName = navigation.getParam('name','no name');
+        let tempRoom = navigation.getParam('room','no name');
         socket = io(ENDPOINT);
 
-        setName(navigation.getParam('name','no name'));
-        setRoom(navigation.getParam('name','no name'));
-        socket.emit('join', { name:'boi', room:'pizza'});
-
-        console.log(name+ ' '+ room);
-        socket.emit('join', { name, room }, (error) => {
+        // setName(navigation.getParam('name','no name'));
+        // setRoom(navigation.getParam('room','no name'));
+        //socket.emit('join', { name:iname, room:iroom});
+        //socket.emit('join', { name:'ASSSSSSSSSSSSS', room:'pizza'});
+        
+        console.log(tempName+ ' '+ navigation.getParam('room','no name'));
+        // console.log('check check check'+ name + ' ' + room);
+        socket.emit('join', { name: tempName, room: tempRoom }, (error) => {
             //Callback if any error to connect to socket server
             if(error){
                 console.log('CONNECTION ERROR');
                 alert(error);
             }
         });
-    }, [ENDPOINT, ]);
+        console.log('client socket has emitted join');
+    }, [ENDPOINT,navigation.getParam('name','no name'), navigation.getParam('room','no name') ]);
 
     // Message Handler
     useEffect(() => {
-        socket.on('message', (message) => {
-          //adding message to all messages
-          setMessages([...messages, message ]);
-        });
+        // socket.on('message', (message) => {
+        //   //adding message to all messages
+        //   console.log(message);
+        //   setMessages([...messages, message ]);
+        // });
     
-        socket.on('roomData', ({ users }) => {
-          setUsers(users);
-        })
+        // socket.on('roomData', ({ users }) => {
+        //     console.log(message);
+        //   setUsers(users);
+        // })
     
         //disconnect event. Server will be listening for disconnect event
         return () => {
@@ -66,18 +78,44 @@ const Chat = ({navigation}) => {
         navigation.navigate('Join', {joinVisible: true});
     }
 
+    const disconnectHandler = () => {
+        socket.emit('disconnect');
+        socket.off();
+    }
+    
+    const debugHandler = () => {
+        let tempName = navigation.getParam('name','no name');
+        let tempRoom = navigation.getParam('room','no name')
+        socket = io(ENDPOINT);
+
+        // setName(tempName);
+        // setRoom(tempRoom);
+        //socket.emit('join', { name:iname, room:iroom});
+        //socket.emit('join', { name:'ASSSSSSSSSSSSS', room:'pizza'});
+        
+        console.log(name+ ' '+ room);
+        socket.emit('join', { tempName, tempRoom }, (error) => {
+            //Callback if any error to connect to socket server
+            if(error){
+                console.log('CONNECTION ERROR');
+                alert(error);
+            }
+        });
+        console.log('client socket has emitted join');
+    }
+
     return(
         <View>
              {/* itemId: {JSON.stringify(navigation.getParam('itemId', 'NO-ID'))} */}
-             <Text>Test Test Test</Text>
+             {/* <Text>Test Test Test</Text> */}
             <Text>Hello {navigation.getParam('name','no name')}</Text>
             <Text>Welcome to {navigation.getParam('room','no name')}</Text>
             <Button title="debug tester"
-                onPress={() => {
-                    setName('asdf');
-                }}/>
+                onPress={debugHandler}/>
             <Button title="back"
                 onPress={backToJoinHandler}/>
+            <Button title="Disconnect" 
+                onPress={disconnectHandler} />
         </View>
     );
 }
